96481fbe12042a5d32f8ed08a74d82e96ec9ff75
Alberto Fanton <alberto.fanton@protonmail.com>
2020-09-12 00:46:32 +0200

    Updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,3 +7,17 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ccls vue-mode use-package try transpose-frame toml-mode toml telega sublime-themes stack-mode spacemacs-theme realgud-ipdb python-pytest pytest pylint pyimpsort pyimport pyenv-mode py-isort py-autopep8 plantuml-mode pdf-tools pdb-mode org-kanban org-journal org-capture-pop-frame ob-ipython material-theme magit lsp-haskell jupyter js-format js-auto-format-mode jedi isortify htmlize flymake-python-pyflakes eslint-fix elpy ein dockerfile-mode docker-compose-mode dante blacken auctex-latexmk))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
