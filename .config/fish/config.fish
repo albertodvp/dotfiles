a1feda9bfd5e58450aa06f4b141d0eaf6644e12c
Alberto Fanton <alberto.fanton@protonmail.com>
2020-09-13 10:42:32 +0200

    Moved to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,10 @@
 set -gx PATH $PATH $HOME/bin $HOME/.cargo/bin $NPM_PACKAGES/bin $HOME/.local/bin
+
+set -gx XDG_DATA_HOME $HOME/data
+
+set -gx  WORKON_HOME $XDG_DATA_HOME/virtualenvs
+
+# ALIASES
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias enw='emacs -nw'
+
