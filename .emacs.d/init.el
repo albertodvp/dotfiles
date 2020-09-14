41908af02658bf81513dc8467b20b1fc1970532b
Alberto Fanton <alberto.fanton@protonmail.com>
2020-09-14 17:08:44 +0200

    Cleanup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,8 +13,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (ccls vue-mode use-package try transpose-frame toml-mode toml telega sublime-themes stack-mode spacemacs-theme realgud-ipdb python-pytest pytest pylint pyimpsort pyimport pyenv-mode py-isort py-autopep8 plantuml-mode pdf-tools pdb-mode org-kanban org-journal org-capture-pop-frame ob-ipython material-theme magit lsp-haskell jupyter js-format js-auto-format-mode jedi isortify htmlize flymake-python-pyflakes eslint-fix elpy ein dockerfile-mode docker-compose-mode dante blacken auctex-latexmk))))
+   '(ccls vue-mode use-package try transpose-frame toml-mode toml telega sublime-themes stack-mode spacemacs-theme realgud-ipdb python-pytest pytest pylint pyimpsort pyimport pyenv-mode py-isort py-autopep8 plantuml-mode pdf-tools pdb-mode org-kanban org-journal org-capture-pop-frame ob-ipython material-theme magit lsp-haskell jupyter js-format js-auto-format-mode jedi isortify htmlize flymake-python-pyflakes eslint-fix elpy ein dockerfile-mode docker-compose-mode dante blacken auctex-latexmk)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
