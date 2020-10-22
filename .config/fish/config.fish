1a4472e34e7c23fa4cb8df8ea9e2ab8b1791f613
Alberto Fanton <alberto.fanton@protonmail.com>
2020-10-22 11:15:47 +0200

    Updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,5 @@ set -gx  WORKON_HOME $XDG_DATA_HOME/virtualenvs
 
 set -gx TERM 'xterm-256color'
 # ALIASES
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias e='emacsclient -t -nw'
-
-
+alias dotfiles '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias e 'emacsclient -t -nw'
