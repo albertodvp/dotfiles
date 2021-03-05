d1efd83fd80a47a8a1bd3027e3b5508b1a0a8446
Alberto Fanton <alberto.fanton@protonmail.com>
2021-03-05 15:21:27 +0100

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,10 +5,14 @@ set -gx XDG_DATA_HOME $HOME/data
 set -gx  WORKON_HOME $XDG_DATA_HOME/virtualenvs
 
 set -gx TERM 'xterm-256color'
+
+set -gx _JAVA_AWT_WM_NONREPARENTING 1
+
 # ALIASES
 alias dotfiles '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias e 'emacsclient -t -nw'
 
 # ghcup-env
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-test -f /home/alberto/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /home/alberto/.ghcup/bin $PATH
+# set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+# test -f /home/alberto/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /home/alberto/.ghcup/bin $PATH
+
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,3 +15,26 @@
  use-package-always-ensure t)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 (garbage-collect)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-disabled-checkers '(haskell-stack-ghc))
+ '(haskell-check-command "hlint")
+ '(haskell-compile-cabal-build-command "stack build")
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type 'stack-ghci)
+ '(haskell-stylish-on-save t)
+ '(initial-frame-alist '((fullscreen . maximized)))
+ '(lsp-ui-doc-use-webkit t)
+ '(package-selected-packages
+   '(org-roam stack-mode doom-modeline all-the-icons which-key vue-html-mode use-package treemacs-projectile transpose-frame toml-mode sublime-themes subatomic256-theme subatomic-theme soothe-theme seoul256-theme realgud rainbow-mode pyimport py-isort py-autopep8 org-journal org-bullets ob-async nyan-mode nord-theme modus-vivendi-theme modus-operandi-theme magit lsp-ui lsp-java lsp-haskell js3-mode importmagic hindent hasklig-mode git-gutter flycheck-mypy flycheck-haskell emojify emoji-cheat-sheet-plus elpy dockerfile-mode docker-compose-mode darktooth-theme ccls blacken auto-complete)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
